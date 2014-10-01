a885a576119ac9d2c9cea9d7c02b7c48c279df9b
Kay Strobach <info@kay-strobach.de>
2014-10-01 14:49:15 +0200

    [TASK] layout

---
+++ Classes/ViewHelpers/Md5ViewHelper.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kay
+ * Date: 01.10.14
+ * Time: 14:29
+ */
+
+namespace KayStrobach\Developer\ViewHelpers;
+
+
+/**
+ * Class Md5ViewHelper
+ *
+ * @package KayStrobach\Developer\ViewHelpers
+ */
+class Md5ViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+	/**
+	 * md5's a string
+	 *
+	 * @param string value
+	 * @return string
+	 */
+	public function render($value) {
+		return md5($value);
+	}
+} 
