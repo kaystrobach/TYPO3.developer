d0f30f610db184563a53598987858a46b01777b3
Kay Strobach <info@kay-strobach.de>
2014-10-06 17:34:36 +0200

    [TASK] check if all listed packages are available

--- Classes/ViewHelpers/Package/IfInstalledViewHelper.php
+++ Classes/ViewHelpers/Package/IfInstalledViewHelper.php
@@ -1,5 +1,6 @@
 <?php
 namespace KayStrobach\Developer\ViewHelpers\Package;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\Flow\Package\PackageManager;
 
 
@@ -21,12 +22,31 @@ class IfInstalledViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractCon
 	 */
 	public function render($package = '') {
 		$this->packageManager->scanAvailablePackages();
-		$package = $this->packageManager->getPackageKeyFromComposerName($package);
-		if($this->packageManager->isPackageAvailable($package)) {
+
+		$packages = GeneralUtility::trimExplode(',', $package);
+
+		$allAvailable = TRUE;
+
+		foreach($packages as $package) {
+			if(!$this->isInstalled($package)) {
+				$allAvailable = FALSE;
+			}
+		}
+
+		if($allAvailable) {
 			return $this->renderThenChild();
 		} else {
 			return $this->renderElseChild();
 		}
+	}
+
+	/**
+	 * @param $package
+	 * @return boolean
+	 */
+	protected function isInstalled($package) {
+		$package = $this->packageManager->getPackageKeyFromComposerName($package);
+		return $this->packageManager->isPackageAvailable($package);
 	}
 
 } 
