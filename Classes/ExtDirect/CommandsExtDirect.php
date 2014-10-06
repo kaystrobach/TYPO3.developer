ff566cb6752409ba38d418b4076bc74009d6b18d
Kay Strobach <info@kay-strobach.de>
2014-10-06 17:34:48 +0200

    [TASK] prepare for ext direct

---
+++ Classes/ExtDirect/CommandsExtDirect.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace KayStrobach\Developer\ExtDirect;
+
+use KayStrobach\Developer\Utility\ShellCaptureUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+
+class CommandsExtDirect {
+
+	public function phpcs($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/phpcs --standard=TYPO3CMS -n ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+
+	public function pdepend($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/pdepend --standard=TYPO3CMS -n ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+
+	public function phpcb($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/phpcb --standard=TYPO3CMS -n ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+
+	public function phpcpd($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/phpcpd ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+
+	public function phpmd($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/phpmd --standard=TYPO3CMS -n ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+
+	public function phpunit($extension) {
+		$cleanedPath = ExtensionManagementUtility::extPath($extension);
+		$command = PATH_site . 'bin/phpunit -ctypo3/sysext/core/Build/UnitTests.xml ' . $cleanedPath;
+		return ShellCaptureUtility::execute($command);
+	}
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,25 @@
 <?php
 
-/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
+/**
+ * Signalslot for adding icons in the extmgm
+ *
+ * @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher
+ */
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
-
-
 $signalSlotDispatcher->connect(
 	'TYPO3\\CMS\\Extensionmanager\\ViewHelpers\\ProcessAvailableActionsViewHelper',
 	'processActions',
 	'KayStrobach\\Developer\\Slots\\ExtensionManager',
 	'processActions'
+);
+
+/**
+ * Register ExtDirect Commands
+ */
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent(
+	'TYPO3.Ajax.ExtDirect.Developer',
+	'KayStrobach\Developer\ExtDirect\CommandsExtDirect',
+	NULL,
+	'user,group'
 );
