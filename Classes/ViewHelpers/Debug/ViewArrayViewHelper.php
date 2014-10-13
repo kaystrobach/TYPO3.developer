0269f521638d5b0ccea1c33687627b30155c7cb8
Kay Strobach <info@kay-strobach.de>
2014-10-13 21:04:09 +0200

    [TASK] add viewArray ViewHelper

---
+++ Classes/ViewHelpers/Debug/ViewArrayViewHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace KayStrobach\Developer\ViewHelpers\Debug;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+
+
+/**
+ * Class ViewArrayViewHelper
+ * @package KayStrobach\Developer\ViewHelpers\Be\Buttons
+ */
+class ViewArrayViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @param mixed $value
+	 * @return string
+	 */
+	public function render($value) {
+		return DebugUtility::viewArray($value);
+	}
+} 
