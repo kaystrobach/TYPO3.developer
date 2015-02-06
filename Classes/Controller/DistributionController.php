c8e42bc78a23137d029761ff0842ce257c1b3618
Kay Strobach <info@kay-strobach.de>
2015-02-06 11:39:37 +0100

    [TASK] Basic distribution exporting

--- Classes/Controller/DistributionController.php
+++ Classes/Controller/DistributionController.php
@@ -1,12 +1,73 @@
 <?php
 
-namespace KayStrobach\Developer\Command;
+namespace KayStrobach\Developer\Controller;
 
 
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\Flow\Utility\Files;
 
 class DistributionController extends ActionController {
-	public function indexAction() {
+	/**
+	 * @var \TYPO3\CMS\Extensionmanager\Utility\ListUtility
+	 * @inject
+	 */
+	protected $listUtility;
 
+	/**
+	 * @var \TYPO3\CMS\Core\Package\PackageManager
+	 * @inject
+	 */
+	protected $packageManager;
+
+	/**
+	 * @param string $extensionName
+	 */
+	public function indexAction($extensionName = '') {
+		$allExtensions = $this->listUtility->enrichExtensionsWithEmConfAndTerInformation($this->listUtility->getAvailableExtensions());
+		$distributions = array();
+
+		// @todo refactor into distribution repo
+		foreach($allExtensions as $extension) {
+			if($extension['category'] === 'distribution') {
+				$distributions[$extension['key']] = (object) $extension;
+			}
+		}
+
+		$this->view->assign('extensions', $distributions);
+		$this->view->assign('extension', $extensionName);
+	}
+
+	public function newAction() {
+
+	}
+
+	public function createAction() {
+
+	}
+
+	public function editAction() {
+
+	}
+
+	/**
+	 * @param string $extensionName
+	 */
+	public function updateAction($extensionName) {
+		try {
+			$distributionPathFileadmin = PATH_site . 'fileadmin/' . $extensionName;
+			$packagePath = $this->packageManager->getPackage($extensionName)->getPackagePath();
+			if(is_dir($packagePath)) {
+				Files::removeDirectoryRecursively($packagePath . 'Initialisation');
+				Files::createDirectoryRecursively($packagePath . 'Initialisation/Files');
+				Files::copyDirectoryRecursively($distributionPathFileadmin, $packagePath . 'Initialisation/Files');
+				$this->addFlashMessage('Copied files from ' . $distributionPathFileadmin . ' to ' . $packagePath . 'Initialisation/Files');
+			}
+		} catch(\Exception $e) {
+			$this->addFlashMessage($e->getMessage(), '', AbstractMessage::ERROR);
+			$this->redirect('index');
+		}
+		$this->redirect('index');
 	}
 }
--- ext_tables.php
+++ ext_tables.php
@@ -6,15 +6,15 @@ if (TYPO3_MODE === 'BE') {
 		'mod1', // Name of the module
 		'', // Position of the module
 		array(// Allowed controller action combinations
-			'Default'       => 'index',
-			'Community'     => 'index,documentation',
-			'Information'   => 'index,listPhpInfo,environmentVariables,hooks,signals,xclass',
-			'Language'      => 'index,xmlToXlf',
-			'Sprite'        => 'listSpriteIcons,regenerateSkinFiles,listTableIcons',
-			'Extension'     => 'index,upload,uploadProcess,autoload,autoloadGenerate,codestylecheck,downloadT3x',
-			'Distributions' => 'index',
-			'Tools'         => 'calculate,diff,,cssanalyze',
-			'System'        => 'clearAllCaches,'
+			'Default'      => 'index',
+			'Community'    => 'index,documentation',
+			'Information'  => 'index,listPhpInfo,environmentVariables,hooks,signals,xclass',
+			'Language'     => 'index,xmlToXlf',
+			'Sprite'       => 'listSpriteIcons,regenerateSkinFiles,listTableIcons',
+			'Extension'    => 'index,upload,uploadProcess,autoload,autoloadGenerate,codestylecheck,downloadT3x',
+			'Distribution' => 'index,new,create,update,edit',
+			'Tools'        => 'calculate,diff,,cssanalyze',
+			'System'       => 'clearAllCaches,'
 		), array(// Additional configuration
 			'access' => 'admin',
 			'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/iconmonstr-wrench-7-icon-16.png',
