8cf268bbc204a1de7182ec006f2dd0790c3c0215
Kay Strobach <info@kay-strobach.de>
2016-04-12 15:48:30 +0200

    [TASK] move to help menu

--- ext_tables.php
+++ ext_tables.php
@@ -2,7 +2,8 @@
 
 if (TYPO3_MODE === 'BE') {
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'KayStrobach.' . $_EXTKEY, 'tools', // Main area
+		'KayStrobach.' . $_EXTKEY,
+		'help', // Main area
 		'mod1', // Name of the module
 		'', // Position of the module
 		array(// Allowed controller action combinations
