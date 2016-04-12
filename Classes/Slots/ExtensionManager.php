47f92628b39db39ea4a6321474b63d243b4f953d
Kay Strobach <info@kay-strobach.de>
2016-04-12 14:29:53 +0200

    [BUGFIX] add excaption catcher for EM

--- Classes/Slots/ExtensionManager.php
+++ Classes/Slots/ExtensionManager.php
@@ -15,33 +15,38 @@ class ExtensionManager {
 	 * @param array $actions
 	 */
 	public function processActions(array $extension, array &$actions) {
-		if($extension['type'] !== 'System') {
-			/** @var \TYPO3\CMS\Fluid\View\StandaloneView $view */
-			$view = GeneralUtility::makeInstance('TYPO3\CMS\Fluid\View\StandaloneView');
-			$view->setTemplatePathAndFilename(ExtensionManagementUtility::extPath('developer') . '/Resources/Private/Templates/Slots/Extensionmanager.html');
-			$view->assignMultiple(
-				array(
-					'extension' => $extension,
-					'actions' => array(
-						array(
-							'icon'       => 'extensions-developer-wrench',
-							'label'      => 'open extension in ext:developer',
-							'action'     => 'index',
-							'controller' => 'Extension',
-						),
-						array(
-							'icon'       => 'actions-document-export-t3d',
-							'label'      => 'Download t3x',
-							'action'     => 'downloadT3x',
-							'controller' => 'Extension',
-						),
+		try {
+			if($extension['type'] !== 'System') {
+				/** @var \TYPO3\CMS\Fluid\View\StandaloneView $view */
+				$view = GeneralUtility::makeInstance('TYPO3\CMS\Fluid\View\StandaloneView');
+				$view->setTemplatePathAndFilename(ExtensionManagementUtility::extPath('developer') . '/Resources/Private/Templates/Slots/Extensionmanager.html');
+				$view->assignMultiple(
+					array(
+						'extension' => $extension,
+						'actions' => array(
+							array(
+								'icon'       => 'extensions-developer-wrench',
+								'label'      => 'open extension in ext:developer',
+								'action'     => 'index',
+								'controller' => 'Extension',
+							),
+							array(
+								'icon'       => 'actions-document-export-t3d',
+								'label'      => 'Download t3x',
+								'action'     => 'downloadT3x',
+								'controller' => 'Extension',
+							),
+						)
 					)
-				)
-			);
-			$buffer = '<span class="t3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-open"></span>';
-			$buffer = $view->render();
-			array_unshift($actions, $buffer);
+				);
+				$buffer = '<span class="t3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-open"></span>';
+				$buffer = $view->render();
+				array_unshift($actions, $buffer);
+			}
+		} catch (\TYPO3Fluid\Fluid\Core\Parser\Exception $e) {
+			$actions[] = '<span class="btn btn default" title="' . htmlspecialchars($e->getMessage()) . '">Problem rendering Developmentplugin</span>';
 		}
+
 	}
 
 }
